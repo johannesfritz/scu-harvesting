8b448e4c019503bde9ee795f0cac5592cbea668f
Johannes Fritz <fritz.johannes@gmail.com>
2022-05-12 06:33:05 -0700

    splitting off navigation exercise + storing the HTML file into 2nd script

--- code/Script - 2  Navigating down the menu and storing the HTML of a page.R
+++ code/Script - 2  Navigating down the menu and storing the HTML of a page.R
@@ -13,32 +13,59 @@ my.session = Session$new(port= my.browser$port)
 my.session$go(my.url)
 
 
-# Looking around the page
-my.session$
-
-## verifying I arrived at the URL
-my.session$getUrl()
-my.session$getTitle()
-
-## Taking screenshots
-## Very useful to check you're navigating right
-my.session$takeScreenshot()
-
-## Navigating on the page
+## Navigating on the page: 
+## Locating the first menu item '117th Congress (2021 - 2022)'
 my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[1]/div')
-## Extracting text (works reasonably well)
-my.target$getText()
 
+## Clicking on it to open the menu itself
 my.target$click()
 
-## extracting the full HTML file
+## checking whether the click worked (top menu should now be open)
+my.session$takeScreenshot()
+
+## (1) Locating the first menu item '117th Congress (2021 - 2022)'
+my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[1]/div')
+my.target$getText() ## Seeing whether I got it
+
+## Clicking on it to open the menu itself
+my.target$click()
+
+
+## (2) Locating the first menu item 'House Hearings'
+my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[2]/div/div[1]/div[1]/div/span')
+my.target$getText() ## Seeing whether I got it
+
+## Clicking on it to open the menu itself
+my.target$click()
+
+
+## (3) Locating the first menu item 'Committee on Education and Labor'
+my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[2]/div/div[1]/div[2]/div/div[4]/div[1]/div/span')
+my.target$getText() ## Seeing whether I got it
+
+## Clicking on it to open the menu itself
+my.target$click()
+
+
+
+## (4) Locating the first DETAILS button for the hearing on ' Clearing the Air: Science-Based Strategies to Protect Workers from Covid-19 Infections'
+my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[2]/div/div[1]/div[2]/div/div[4]/div[2]/div/table[1]/tbody/tr/td[2]/div/a[3]')
+my.target$getText() ## Seeing whether I got it
+
+## Clicking on it to open the menu itself
+my.target$click()
+my.session$getUrl() ## did we get to the right location?
+
+
+
+## extracting the full HTML file into the 'data' folder
 html = my.session$getSource()
 
-html=XML::htmlParse(html, asText=T)
 writeLines(text = html, con = 'data/my first page.html')
 
 
-
+## closing the session
+my.session$delete()
 
 
 
