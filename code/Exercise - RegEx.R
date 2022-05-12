cc2b5f5cdf7ae5cf9f9998df17182ebca804e539
Johannes Fritz <fritz.johannes@gmail.com>
2022-05-12 13:51:01 -0700

    updated article

--- code/Exercise - RegEx.R
+++ code/Exercise - RegEx.R
@@ -1,45 +1,12 @@
-library(webdriver)
-library(XML)
+# please install httr
+# install.packages("stringr")
 
-## starting the headless browser
-my.browser = run_phantomjs()
-my.browser
+library(stringr)
 
+# an article about basketball salaries:
+# https://thesportjournal.org/article/determinants-of-nba-player-salaries/
+practice.text="In the 2013-2014 National Basketball Association (NBA) season, the Los Angeles Laker’s Kobe Bryant earned $30.4 million. That same year Dirk Nowitzki, Amar’e Stoudemire, Joe Johnson, Carmello Anthony, and Dwight Howard earned $22.2, $21.7, $21.4, $21.3, and $20.5 million respectively (8). The average salary of an NBA player for the 2013-2014 season was $5.1 million which was higher than the average salary for a National Football League, National Hockey League, or Major League Baseball player (3). What’s more, 52 (9.1%) of NBA players earned between $10 million and $19 million during the 2013-2014 season (8). The NBA players’ salary cap was instituted in the 1984-1985 season. That year the salary cap was $3.6 million and the average player salary was $300,000 (5). In 1984-1985 eleven NBA players made $1 million or more with the Lakers’ Earvin “Magic” Johnson topping the list with $2.5 million (1). In 2013, 377 (66.2%) of NBA players earned $1 million or more. For the 2012-2013 season, the NBA salary cap was $58 million and the average NBA player salary was $5.1 million. Again, in the 1984-1985 season, Magic Johnson’s $2.5 million was the highest salary in the NBA. However, in the 2013-2014 the highest salary was Los Angeles Lakers’ Kobe Bryant who made $30.4 million.
 
-# Opening a session and accessing the URL
-my.url="https://www.govinfo.gov/app/collection/chrg/"
-my.session = Session$new(port= my.browser$port)
+The escalation of NBA player salaries has not only been explosive, but it has also created a large earnings gap between players. Kobe Bryant’s 2013-2014 $30.4 million salary considerably dwarfed his Lakers teammate Ryan Kelly’s $490,000. There has been considerable attention paid to the escalation of NBA player salaries (24).  Sprow (25) seemed to think that there is a salary bubble in the NBA and that salaries will decrease or come “tumbling down” very soon. There may be evidence for this. From the 1984-1985 to 2006-2007, the average NBA player salary continued to increase. However, from 2007-2008 until 2012-2013, NBA salaries leveled off (3, 13).
 
-my.session$go(my.url)
-Sys.sleep(2)
-
-## Navigating to the list of House Committees
-
-my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[1]/div')
-my.target$click()
-Sys.sleep(.5)
-
-my.target= my.session$findElement(xpath='/html/body/div[10]/div[6]/div/div[3]/div/div/div[1]/div[2]/div/div[1]/div[1]/div/span')
-my.target$click()
-Sys.sleep(.5)
-
-
-## Find the common root for your XPaths and use it to get all committee names at once
-
-common.root='' # paste solution here
-
-all.committees=my.session$findElements(xpath=common.root) # should give you a list with 19 elements
-
-
-## Now add the for loop printing the full list of House Committee names by looping through the elemtns
-
-for(){ ## <-- add variable you are looping over and its dimension
-  
-  print(all.committees$getText()) ## <-- change to respond to the variable you are looping over
-  
-}
-
-
-
-## closing the session
-my.session$delete()
+What’s more intriguing is the fact that some question whether NBA players are overpaid. Nathan (22) indicated that overpayment in the NBA is problematic. Statistically some players are overpaid based on poor or sub-par performance. These players are highly sought after because they are proficient in one aspect of their game such as three point shooting or they may be a defensive force. However, these same specialized players may be a major liability in other areas i.e., the three point shooter may be a poor defender and the defensive force may be a poor free throw shooter. Whatever the issue, overpayment in the NBA is a concern. The reason for payment is to fairly compensate a player for his play. However, in the NBA it seems as if compensation is a reward for past performance and anticipated or expected future performance. However, past performance and expected performance may not be good indicators of fair compensation. NBA owners and general managers often over spend for a player that they feel will meet and immediate need. This is very myopic."
